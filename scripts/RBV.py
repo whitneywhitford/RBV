0dd47c15da5cdd730ff8cd7a2fd7f632f98d4a3d
whitneywhitford <whitney.whitford@auckland.ac.nz>
2017-04-21 12:57:25 +1200

    working with sys.argv arguments

--- scripts/RBV.py
+++ scripts/RBV.py
@@ -10,109 +10,125 @@ from scipy import stats
 import vcf
 import numpy as np
 import make_random_windows
+from shutil import copyfile
+
+
+
+gapfile = sys.argv[1]
+CNV_file = sys.argv[2]
+variant_permutations = int(sys.argv[3])
+vcf_file = sys.argv[4]
+qualCutoff = int(sys.argv[5])
+out_file = sys.argv[6]
+window_permutations = int(sys.argv[7])
+ref_file = sys.argv[8]
 
 
 
 
-def parse_args():
-	"""
-	Parse command-line arguments
 
-	Returns
-	-------
 
-	Parser argument namespace
-	"""
-	parser = argparse.ArgumentParser(description="RBV")
 
-	parser.add_argument(
-		"--ref", required=True,
-		help="REQUIRED. Path to reference sequence (including file name).")
 
-	parser.add_argument(
-		"--output_dir", "-o", default="RBV",
-		help="REQUIRED. Output directory. XYalign will create a directory "
-		"structure within this directory")
+#def parse_args():
+"""
+Parse command-line arguments
 
-	parser.add_argument(
-		"--sample_id", "-id", default="sample",
-		help="Name/ID of sample - for use in plot titles and file naming. "
-		"Default is sample")
+Returns
+-------
 
-	"""parser.add_argument(
-		"--cpus", type=int, default=1,
-		help="Number of cores/threads to use. Default is 1")
+Parser argument namespace
+"""
+"""parser = argparse.ArgumentParser(description="RBV")
 
-	parser.add_argument(
-		"--xmx", default="None",
-		help="Memory to be provided to java programs via -Xmx.  E.g., use the "
-		"flag '--xmx Xmx4g' to pass '-Xmx4g' as a flag when running java "
-		"programs (currently just repair.sh). Default is 'None' (i.e., nothing "
-		"provided on the command line), which will allow repair.sh to "
-		"automatically allocate memory.")
+parser.add_argument(
+	"--ref", required=True,
+	help="REQUIRED. Path to reference sequence (including file name).")
 
-	parser.add_argument(
-		"--single_end", action="store_true", default=False,
-		help="Include flag if reads are single-end and NOT paired-end.")"""
+parser.add_argument(
+	"--output_dir", "-o", default="RBV",
+	help="REQUIRED. Output directory. XYalign will create a directory "
+	"structure within this directory")
 
-	# Logging options
-	parser.add_argument(
-		"--logfile", default=None,
-		help="Name of logfile.  Will overwrite if exists.  Default is "
-		"sample_xyalign.log")
+parser.add_argument(
+	"--sample_id", "-id", default="sample",
+	help="Name/ID of sample - for use in plot titles and file naming. "
+	"Default is sample")"""
 
-	parser.add_argument(
-		"--reporting_level", default='INFO',
-		choices=["DEBUG", "INFO", "ERROR", "CRITICAL"],
-		help="Set level of messages printed to console. Default is 'INFO'. "
-		"Choose from (in decreasing amount of reporting) DEBUG, INFO, ERROR "
-		"or CRITICAL")
+"""parser.add_argument(
+	"--cpus", type=int, default=1,
+	help="Number of cores/threads to use. Default is 1")
 
-	# Options for turning on/off parts of the pipeline
-	parser.add_argument(
-		"--prepare_vcf", action="store_true", default=False,
-		help="This flag will result in the preparation of a vcf file by "
-		"HaploTypeCaller. A HaploTypeCaller generated vcf file can be parsed "
-		"using the --vcf flag.")
+parser.add_argument(
+	"--xmx", default="None",
+	help="Memory to be provided to java programs via -Xmx.  E.g., use the "
+	"flag '--xmx Xmx4g' to pass '-Xmx4g' as a flag when running java "
+	"programs (currently just repair.sh). Default is 'None' (i.e., nothing "
+	"provided on the command line), which will allow repair.sh to "
+	"automatically allocate memory.")
 
-	# Variant Calling Flags
-	parser.add_argument(
-		"--variant_quality_cutoff", "-vqc", type=int, default=20,
-		help="Consider all SNPs with a quality greater than or "
-		"equal to this value. Default is 20.")
+parser.add_argument(
+	"--single_end", action="store_true", default=False,
+	help="Include flag if reads are single-end and NOT paired-end.")"""
 
-	# Bam Analysis and Characterize Sex Chrom Flags
-	parser.add_argument(
-		"--window_size", "-w", default=None,
-		help="Window size (integer) for sliding window calculations. Default "
-		"is 50000.  Default is None. If set to None, will use targets "
-		"provided using --target_bed.")
+"""# Logging options
+parser.add_argument(
+	"--logfile", default=None,
+	help="Name of logfile.  Will overwrite if exists.  Default is "
+	"sample_xyalign.log")
 
-	parser.add_argument(
-		"--CNV_bed", default=None,
-		help="Bed file containing targets to use in CNV analyses "
-		"Must be typical bed format, 0-based indexing, with the first three "
-		"columns the chromosome name, start coordinate, stop coordinate.")
+parser.add_argument(
+	"--reporting_level", default='INFO',
+	choices=["DEBUG", "INFO", "ERROR", "CRITICAL"],
+	help="Set level of messages printed to console. Default is 'INFO'. "
+	"Choose from (in decreasing amount of reporting) DEBUG, INFO, ERROR "
+	"or CRITICAL")
 
-	parser.add_argument(
-		"--gap_bed", default=None,
-		help="Bed file containing gaps in the reference to mask for random "
-		"generation. Must be typical bed format, 0-based indexing, with the "
-		"first three columns the chromosome name, start coordinate, stop "
-		"coordinate.")
-		
-	parser.add_argument(
-		"--variant_permutations", type=int, default=10000,
-		help="Number of permutations to use for heterozygous read balance "
-		"analyses. Default is 10000")
+# Options for turning on/off parts of the pipeline
+parser.add_argument(
+	"--prepare_vcf", action="store_true", default=False,
+	help="This flag will result in the preparation of a vcf file by "
+	"HaploTypeCaller. A HaploTypeCaller generated vcf file can be parsed "
+	"using the --vcf flag.")
 
-	parser.add_argument(
-		"--window_permutations", type=int, default=10000,
-		help="Number of permutations to use for heterozygous SNP number "
-		"analyses. Default is 10000")
+# Variant Calling Flags
+parser.add_argument(
+	"--variant_quality_cutoff", "-vqc", type=int, default=20,
+	help="Consider all SNPs with a quality greater than or "
+	"equal to this value. Default is 20.")
 
-	# Return arguments namespace
-	return args
+# Bam Analysis and Characterize Sex Chrom Flags
+parser.add_argument(
+	"--window_size", "-w", default=None,
+	help="Window size (integer) for sliding window calculations. Default "
+	"is 50000.  Default is None. If set to None, will use targets "
+	"provided using --target_bed.")
+
+parser.add_argument(
+	"--CNV_bed", default=None,
+	help="Bed file containing targets to use in CNV analyses "
+	"Must be typical bed format, 0-based indexing, with the first three "
+	"columns the chromosome name, start coordinate, stop coordinate.")
+
+parser.add_argument(
+	"--gap_bed", default=None,
+	help="Bed file containing gaps in the reference to mask for random "
+	"generation. Must be typical bed format, 0-based indexing, with the "
+	"first three columns the chromosome name, start coordinate, stop "
+	"coordinate.")
+	
+parser.add_argument(
+	"--variant_permutations", type=int, default=10000,
+	help="Number of permutations to use for heterozygous read balance "
+	"analyses. Default is 10000")
+
+parser.add_argument(
+	"--window_permutations", type=int, default=10000,
+	help="Number of permutations to use for heterozygous SNP number "
+	"analyses. Default is 10000")
+
+# Return arguments namespace
+return args"""
 
 
 	
@@ -244,7 +260,7 @@ def empirical_pvalue(window_het_snps, vcf_outfile, num_window_samples):
 	
 	sample_cum_sum_pvalue = sample_cum_sum / num_window_samples
 	
-	return sample_het_snps,cum_sum_pvalue
+	return sample_het_snps,sample_cum_sum_pvalue
 
 def readbal(variant_lines,qualCutoff):
 	
@@ -279,18 +295,25 @@ def readbal(variant_lines,qualCutoff):
 			# Add to arrays
 			readBalance.append(ReadRatio)
 			
-	return readbal
+	return readBalance
 
 	
 def random_readbal(no_of_samples,vcf_file,gaps,qualCutoff):
 	
 	variant_lines = rand_het_sites(no_of_samples,vcf_file,gaps)
 	
-	random_readbal = read_bal(variant_lines,qualCutoff)
+	random_readbal = readbal(variant_lines,qualCutoff)
 	
 	return random_readbal
 
-
+def prep_vcf(vcf_file):
+	working_vcf = "working_vcf.vcf"
+	copyfile(vcf_file, working_vcf)
+	os.system("bgzip " + working_vcf)
+	bgzip_working_vcf = str(working_vcf) + ".gz"
+	os.system("tabix -p vcf " + bgzip_working_vcf)
+	
+	return bgzip_working_vcf
 
 
 	
@@ -305,18 +328,18 @@ stats.ttest_ind(sample_readbal, random_readbal, equal_var=False)
 stats.ks_2samp(sample_readbal, random_readbal)"""
 
 
-def main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff, window_permutations, windows_size, out_file):
+def main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff, window_permutations, out_file):
 
 	ref_fai = str(ref_file) + ".fai"	#check if all fai are just fasta.fai/fa.fai
 	
 	#if HC needed
 
-	#bgzip and tabix via pysam
+
 	
 	
 	out = open(out_file, 'w')
-	print >>out, "# RBV: Read balance validator"
-	print >>out, "# " + str(sys.arg)
+	print >>out, "#RBV: Read balance validator"
+	print >>out, "#Command: " + str(sys.argv)
 	
 	
 	
@@ -325,12 +348,14 @@ def main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff
 	
 	
 	#random readbal - perform once per run of RBV
-	gap_sites = gaps(gapfile,CNVfile)
-	rand_readbal = random_readbal(variant_permutations,vcf_file,gaps,qualCutoff)
+	bzip_vcf = prep_vcf(vcf_file)	#check if need to bgzip and tabix
+	
+	gap_sites = gaps(gapfile,CNV_file)
+	rand_readbal = random_readbal(variant_permutations,vcf_file,gap_sites,qualCutoff)
 	rand_readbal_array = np.array(rand_readbal).reshape(len(rand_readbal));
 	rand_mean = np.mean(rand_readbal_array)
 	
-	print >>out, "# Random mean read balance = " + str(rand_mean)
+	print >>out, "#Random mean read balance = " + str(rand_mean)
 	print >>out, "#"
 	print >>out, "#CHR\tSTART\tSTOP\th1 het snp number\th1 pvalue\th3 mean readbal\th3 t-test\th3 ks-test"
 	
@@ -338,21 +363,27 @@ def main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff
 	
 	#For each CNV
 	for i in range(len(CNVs)):
-		CNV_chr,CNV_start,CNV_stop,CNV_type=CNVs[i].strip().split()
+		CNV_chr,raw_CNV_start,raw_CNV_stop,CNV_type=CNVs[i].strip().split()
+		CNV_start = int(raw_CNV_start)
+		CNV_stop = int(raw_CNV_stop)
 		
 		out.write(str(CNV_chr) + "\t" + str(CNV_start) + "\t" + str(CNV_stop) + "\t")
 		
 		#DEL
 		CNV_vcf_file = "CNV_vcf_file_" + str(i) + ".vcf"
-		vcf_fetch(vcf_file,CNV_vcf_file, chr, start, stop)
+		vcf_fetch(bzip_vcf,CNV_vcf_file, CNV_chr, CNV_start, CNV_stop)
 		
+		windows_size = CNV_stop - CNV_start
 		
 		random_windows = make_random_windows.main(ref_fai, gap_sites, window_permutations, windows_size, i)
 		window_het_count = []
-		for w in range(len(random_windows)):
-			window_count,window_chr,window_start,window_end = window.split()
+		for w in range(len(random_windows)):		
+			window_chr,raw_window_start,raw_window_end,window_count= random_windows[w].split()
+			window_start = int(raw_window_start)
+			window_end = int(raw_window_end)
+			
 			window_vcf_file = "rand_vcf_file_" + str(i) + "_" + str(w) + ".vcf"
-			vcf_fetch(vcf_file,window_vcf_file, window_chr, window_start, window_end)
+			vcf_fetch(bzip_vcf,window_vcf_file, window_chr, window_start, window_end)
 			window_het_count.append(het_count(window_vcf_file))
 			os.remove(window_vcf_file)
 		
@@ -370,8 +401,8 @@ def main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff
 		CNV_readbal_array = np.array(CNV_readbal).reshape(len(CNV_readbal))
 		
 		CNV_mean = np.mean(CNV_readbal_array)
-		CNV_ttest_pvalue = stats.ttest_ind(CNV_readbal_array, rand_readbal_array, equal_var=False)
-		CNV_kstest_pvalue = stats.ks_2samp(CNV_readbal_array, rand_readbal_array)
+		CNV_ttest_stat,CNV_ttest_pvalue = stats.ttest_ind(CNV_readbal_array, rand_readbal_array, equal_var=False)
+		CNV_kstest_stat,CNV_kstest_pvalue = stats.ks_2samp(CNV_readbal_array, rand_readbal_array)
 		
 		os.remove(CNV_vcf_file)
 		
@@ -383,13 +414,14 @@ def main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff
 		
 		
 		
-		
+	out.close()
 		
 		
 		
 
 
-
+if __name__=='__main__':
+	main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff, window_permutations, out_file)
 
 
 
