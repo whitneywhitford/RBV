c19a0e7eca219a4fdbb03c8881b4653cab204f73
whitneywhitford <whitney.whitford@auckland.ac.nz>
2017-04-26 16:59:58 +1200

    output now to output dir including tmp dir

--- scripts/RBV.py
+++ scripts/RBV.py
@@ -11,6 +11,7 @@ import vcf
 import numpy as np
 import make_random_windows
 from shutil import copyfile
+from shutil import rmtree
 from datetime import datetime
 
 
@@ -21,9 +22,10 @@ CNV_file = sys.argv[2]
 variant_permutations = int(sys.argv[3])
 vcf_file = sys.argv[4]
 qualCutoff = int(sys.argv[5])
-out_file = sys.argv[6]
+sample_id = sys.argv[6]
 window_permutations = int(sys.argv[7])
 ref_file = sys.argv[8]
+out_dir = sys.argv[9]
 
 
 
@@ -32,105 +34,63 @@ ref_file = sys.argv[8]
 
 
 
-#def parse_args():
-"""
-Parse command-line arguments
+def parse_args():
+	"""Parse command-line arguments"""
 
-Returns
--------
+	parser = argparse.ArgumentParser(description="RBV")
 
-Parser argument namespace
-"""
-"""parser = argparse.ArgumentParser(description="RBV")
+	parser.add_argument(
+		"--ref", required=True,
+		help="REQUIRED. Path to reference sequence (including file name).")
 
-parser.add_argument(
-	"--ref", required=True,
-	help="REQUIRED. Path to reference sequence (including file name).")
+	parser.add_argument(
+		"--output_dir", "-o", default="RBV",
+		help="Output directory. RBV will create a temporary "
+		"directory and output file within this directory.")
 
-parser.add_argument(
-	"--output_dir", "-o", default="RBV",
-	help="REQUIRED. Output directory. XYalign will create a directory "
-	"structure within this directory")
+	parser.add_argument(
+		"--sample_id", "-id", default="sample",
+		help="Name/ID of sample - for use in plot titles and file naming. "
+		"Default is sample")
 
-parser.add_argument(
-	"--sample_id", "-id", default="sample",
-	help="Name/ID of sample - for use in plot titles and file naming. "
-	"Default is sample")"""
+	# Variant Calling Flags
+	parser.add_argument(
+		"--variant_quality_cutoff", "-vqc", type=int, default=20,
+		help="Consider all SNPs with a quality greater than or "
+		"equal to this value. Default is 20.")
 
-"""parser.add_argument(
-	"--cpus", type=int, default=1,
-	help="Number of cores/threads to use. Default is 1")
 
-parser.add_argument(
-	"--xmx", default="None",
-	help="Memory to be provided to java programs via -Xmx.  E.g., use the "
-	"flag '--xmx Xmx4g' to pass '-Xmx4g' as a flag when running java "
-	"programs (currently just repair.sh). Default is 'None' (i.e., nothing "
-	"provided on the command line), which will allow repair.sh to "
-	"automatically allocate memory.")
+	parser.add_argument(
+		"--CNV_bed", required=True,
+		help="REQUIRED. Bed file containing targets to use in CNV analyses "
+		"Must be typical bed format, 0-based indexing, with the first four "
+		"columns the chromosome name, start coordinate, stop coordinate,"
+		"and predicted CNV type.")
 
-parser.add_argument(
-	"--single_end", action="store_true", default=False,
-	help="Include flag if reads are single-end and NOT paired-end.")"""
+	parser.add_argument(
+		"--gap_bed", required=True,
+		help="REQUIRED. Bed file containing gaps in the reference to mask "
+		"for random generation. Must be typical bed format, 0-based indexing,"
+		"with the first four columns the chromosome name, start coordinate,"
+		"stop coordinate, and predicted type.")
+		
+	parser.add_argument(
+		"--vcf", required=True,
+		help="REQUIRED. VCF file containing the variants for RBV analyses."
+		"VCF must be generated using HaploTypeCaller.")
+		
+	parser.add_argument(
+		"--variant_permutations", type=int, default=10000,
+		help="Number of permutations to use for heterozygous read balance "
+		"analyses. Default is 10000")
 
-"""# Logging options
-parser.add_argument(
-	"--logfile", default=None,
-	help="Name of logfile.  Will overwrite if exists.  Default is "
-	"sample_xyalign.log")
+	parser.add_argument(
+		"--window_permutations", type=int, default=1000,
+		help="Number of permutations to use for read balance analyses."
+		"Default is 1000")
 
-parser.add_argument(
-	"--reporting_level", default='INFO',
-	choices=["DEBUG", "INFO", "ERROR", "CRITICAL"],
-	help="Set level of messages printed to console. Default is 'INFO'. "
-	"Choose from (in decreasing amount of reporting) DEBUG, INFO, ERROR "
-	"or CRITICAL")
-
-# Options for turning on/off parts of the pipeline
-parser.add_argument(
-	"--prepare_vcf", action="store_true", default=False,
-	help="This flag will result in the preparation of a vcf file by "
-	"HaploTypeCaller. A HaploTypeCaller generated vcf file can be parsed "
-	"using the --vcf flag.")
-
-# Variant Calling Flags
-parser.add_argument(
-	"--variant_quality_cutoff", "-vqc", type=int, default=20,
-	help="Consider all SNPs with a quality greater than or "
-	"equal to this value. Default is 20.")
-
-# Bam Analysis and Characterize Sex Chrom Flags
-parser.add_argument(
-	"--window_size", "-w", default=None,
-	help="Window size (integer) for sliding window calculations. Default "
-	"is 50000.  Default is None. If set to None, will use targets "
-	"provided using --target_bed.")
-
-parser.add_argument(
-	"--CNV_bed", default=None,
-	help="Bed file containing targets to use in CNV analyses "
-	"Must be typical bed format, 0-based indexing, with the first three "
-	"columns the chromosome name, start coordinate, stop coordinate.")
-
-parser.add_argument(
-	"--gap_bed", default=None,
-	help="Bed file containing gaps in the reference to mask for random "
-	"generation. Must be typical bed format, 0-based indexing, with the "
-	"first three columns the chromosome name, start coordinate, stop "
-	"coordinate.")
-	
-parser.add_argument(
-	"--variant_permutations", type=int, default=10000,
-	help="Number of permutations to use for heterozygous read balance "
-	"analyses. Default is 10000")
-
-parser.add_argument(
-	"--window_permutations", type=int, default=10000,
-	help="Number of permutations to use for heterozygous SNP number "
-	"analyses. Default is 10000")
-
-# Return arguments namespace
-return args"""
+	# Return arguments namespace
+	return args
 
 
 	
@@ -308,8 +268,9 @@ def random_readbal(no_of_samples,vcf_file,gaps,qualCutoff):
 	
 	return random_readbal
 
-def prep_vcf(vcf_file):
-	working_vcf = "working_vcf.vcf"
+def prep_vcf(vcf_file, out_dir):
+	working_vcf = os.path.join(out_dir, "tmp", "working_vcf.vcf")
+	
 	copyfile(vcf_file, working_vcf)
 	os.system("bgzip " + working_vcf)
 	bgzip_working_vcf = str(working_vcf) + ".gz"
@@ -318,124 +279,132 @@ def prep_vcf(vcf_file):
 	return bgzip_working_vcf
 
 
-	
-	
-	
-	
-"""np.mean(random_readbal)	#once
-
-#Each CNV
-np.mean(sample_readbal)
-stats.ttest_ind(sample_readbal, random_readbal, equal_var=False)
-stats.ks_2samp(sample_readbal, random_readbal)"""
 
 
-def main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff, window_permutations, out_file):
+def main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff, window_permutations, sample_id, out_dir):
 
-	ref_fai = str(ref_file) + ".fai"	#check if all fai are just fasta.fai/fa.fai
+	#setup output directories
+	if os.path.isdir(out_dir):
+		print "[" + str(datetime.now()) + "] Output directory already exists. Please enter new Output directory"
 	
-	if not os.path.isfile(ref_fai):
-		os.system("samtools faidx  " + ref_file)
-		ref_fai_base = os.path.basename(ref_fai)
-		os.rename(ref_fai, """ "path/to/new/desination/for/" +""" ref_fai_base)
-	
-	#if HC needed
+	else:
+		os.mkdir(out_dir)
+		tmp = str(out_dir) + "/tmp"
+		os.mkdir(tmp)
 
 
-	
-	
-	out = open(out_file, 'w')
-	print >>out, "#RBV: Read balance validator"
-	print >>out, "#Command: " + str(sys.argv)
-	
-	
-	
-	
-	
-	
-	
-	#random readbal - perform once per run of RBV
-	bzip_vcf = prep_vcf(vcf_file)	#check if need to bgzip and tabix
-	
-	gap_sites = gaps(gapfile,CNV_file)
-	rand_readbal = random_readbal(variant_permutations,vcf_file,gap_sites,qualCutoff)
-	rand_readbal_array = np.array(rand_readbal).reshape(len(rand_readbal));
-	rand_mean = np.mean(rand_readbal_array)
-	
-	print >>out, "#Random mean read balance = " + str(rand_mean)
-	print >>out, "#"
-	print >>out, "#CHR\tSTART\tSTOP\th1 het snp number\th1 pvalue\th3 mean readbal\th3 t-test\th3 ks-test"
-	
-	CNVs=open(CNV_file).readlines()
-	
-	#For each CNV
-	for i in range(len(CNVs)):
-		CNV_chr,raw_CNV_start,raw_CNV_stop,CNV_type=CNVs[i].strip().split()
-		CNV_start = int(raw_CNV_start)
-		CNV_stop = int(raw_CNV_stop)
+		ref_fai = str(ref_file) + ".fai"	#check if all fai are just fasta.fai/fa.fai
 		
-		out.write(str(CNV_chr) + "\t" + str(CNV_start) + "\t" + str(CNV_stop) + "\t")
-		
-		#DEL
-		CNV_vcf_file = "CNV_vcf_file_" + str(i) + ".vcf"
-		vcf_fetch(bzip_vcf,CNV_vcf_file, CNV_chr, CNV_start, CNV_stop)
-		
-		windows_size = CNV_stop - CNV_start
-		
-		permutation = i + 1
-		
-		random_windows = make_random_windows.main(ref_fai, gap_sites, window_permutations, windows_size, permutation)
-		print "[" + str(datetime.now()) + "] Random window generation - CNV"+str(permutation)+" complete."
-		
-		window_het_count = []
-		for w in range(len(random_windows)):		
-			window_chr,raw_window_start,raw_window_end,window_count= random_windows[w].split()
-			window_start = int(raw_window_start)
-			window_end = int(raw_window_end)
+		if not os.path.isfile(ref_fai):
+			os.system("samtools faidx  " + ref_file)
+			ref_fai_base = os.path.basename(ref_fai)
 			
-			window_vcf_file = "rand_vcf_file_" + str(i) + "_" + str(w) + ".vcf"
-			vcf_fetch(bzip_vcf,window_vcf_file, window_chr, window_start, window_end)
-			window_het_count.append(het_count(window_vcf_file))
-			os.remove(window_vcf_file)
+			old_ref_fai = ref_fai
+			ref_fai = os.path.join(out_dir, "tmp", ref_fai_base)
+			
+			os.rename(old_ref_fai, ref_fai)
 		
-		if all([ v == 0 for v in window_het_count ]):
-			out.write("0\tnan\t")
-			print "[" + str(datetime.now()) + "] CNV" + str(permutation) + ": all random windows of this length contain no heterozygous SNPs. CNV too small or too few permuations"
-			CNV_het_count = het_count(CNV_vcf_file)
-			
-		else:
-			CNV_het_count,CNV_emp_pvalue = empirical_pvalue(window_het_count, CNV_vcf_file, window_permutations)
-			
-			out.write(str(CNV_het_count) + "\t" + str(CNV_emp_pvalue) + "\t")
+		out_file = os.path.join(out_dir, "{}_RBV.txt".format(sample_id))
+		
+		#if HC needed
 
 
-		#DUP
-		if CNV_het_count == 0:
-			out.write("nan\tnan\tnan\n")
-			print "[" + str(datetime.now()) + "] CNV" + str(permutation) + ": contains no heterozygous SNPs, unable to perform duplication analyses"
-		else:
-			CNV_vcf_lines=open(CNV_vcf_file).readlines()
-			CNV_readbal = readbal(CNV_vcf_lines,qualCutoff)
-			CNV_readbal_array = np.array(CNV_readbal).reshape(len(CNV_readbal))
+		
+		
+		out = open(out_file, 'w')
+		print >>out, "#RBV: Read balance validator"
+		print >>out, "#Command: " + str(sys.argv)
+		
+		
+		
+		
+		
+		
+		
+		#random readbal - perform once per run of RBV
+		bzip_vcf = prep_vcf(vcf_file, out_dir)	#check if need to bgzip and tabix
+		
+		gap_sites = gaps(gapfile,CNV_file)
+		rand_readbal = random_readbal(variant_permutations,vcf_file,gap_sites,qualCutoff)
+		rand_readbal_array = np.array(rand_readbal).reshape(len(rand_readbal));
+		rand_mean = np.mean(rand_readbal_array)
+		
+		print >>out, "#Random mean read balance = " + str(rand_mean)
+		print >>out, "#"
+		print >>out, "#CHR\tSTART\tSTOP\tpredicted type\th1 het snp number\th1 pvalue\th3 mean readbal\th3 t-test\th3 ks-test"
+		
+		CNVs=open(CNV_file).readlines()
+		
+		#For each CNV
+		for i in range(len(CNVs)):
+			CNV_chr,raw_CNV_start,raw_CNV_stop,CNV_type=CNVs[i].strip().split()
+			CNV_start = int(raw_CNV_start)
+			CNV_stop = int(raw_CNV_stop)
 			
-			CNV_mean = np.mean(CNV_readbal_array)
-			CNV_ttest_stat,CNV_ttest_pvalue = stats.ttest_ind(CNV_readbal_array, rand_readbal_array, equal_var=False)
-			CNV_kstest_stat,CNV_kstest_pvalue = stats.ks_2samp(CNV_readbal_array, rand_readbal_array)
+			out.write(str(CNV_chr) + "\t" + str(CNV_start) + "\t" + str(CNV_stop) + "\t" + str(CNV_type) + "\t")
+			
+			permutation = i + 1
+			
+			#DEL
+			CNV_vcf_filename = str(sample_id) + "_CNV" + str(permutation) + ".vcf"
+			CNV_vcf_file = os.path.join(out_dir, "tmp", CNV_vcf_filename)
+			vcf_fetch(bzip_vcf,CNV_vcf_file, CNV_chr, CNV_start, CNV_stop)
+			
+			windows_size = CNV_stop - CNV_start
+			
+			random_windows = make_random_windows.main(ref_fai, gap_sites, window_permutations, windows_size, permutation)
+			print "[" + str(datetime.now()) + "] Random window generation - CNV"+str(permutation)+" complete."
+			
+			window_het_count = []
+			for w in range(len(random_windows)):		
+				window_chr,raw_window_start,raw_window_end,window_count= random_windows[w].split()
+				window_start = int(raw_window_start)
+				window_end = int(raw_window_end)
+				
+				window_vcf_filename = "rand_vcf_file_" + str(permutation) + "_" + str(w) + ".vcf"
+				window_vcf_file = os.path.join(out_dir, "tmp", window_vcf_filename)
+				vcf_fetch(bzip_vcf,window_vcf_file, window_chr, window_start, window_end)
+				window_het_count.append(het_count(window_vcf_file))
+				os.remove(window_vcf_file)
+			
+			if all([ v == 0 for v in window_het_count ]):
+				out.write("0\tnan\t")
+				print "[" + str(datetime.now()) + "] CNV" + str(permutation) + ": all random windows of this length contain no heterozygous SNPs. CNV too small or too few permuations"
+				CNV_het_count = het_count(CNV_vcf_file)
+				
+			else:
+				CNV_het_count,CNV_emp_pvalue = empirical_pvalue(window_het_count, CNV_vcf_file, window_permutations)
+				
+				out.write(str(CNV_het_count) + "\t" + str(CNV_emp_pvalue) + "\t")
+
+
+			#DUP
+			if CNV_het_count == 0:
+				out.write("nan\tnan\tnan\n")
+				print "[" + str(datetime.now()) + "] CNV" + str(permutation) + ": contains no heterozygous SNPs, unable to perform duplication analyses"
+			else:
+				CNV_vcf_lines=open(CNV_vcf_file).readlines()
+				CNV_readbal = readbal(CNV_vcf_lines,qualCutoff)
+				CNV_readbal_array = np.array(CNV_readbal).reshape(len(CNV_readbal))
+				
+				CNV_mean = np.mean(CNV_readbal_array)
+				CNV_ttest_stat,CNV_ttest_pvalue = stats.ttest_ind(CNV_readbal_array, rand_readbal_array, equal_var=False)
+				CNV_kstest_stat,CNV_kstest_pvalue = stats.ks_2samp(CNV_readbal_array, rand_readbal_array)
+					
+				out.write(str(CNV_mean) + "\t" + str(CNV_ttest_pvalue) + "\t" + str(CNV_kstest_pvalue) + "\n")
 			
 			os.remove(CNV_vcf_file)
+		
+		rmtree(tmp)
 			
-			out.write(str(CNV_mean) + "\t" + str(CNV_ttest_pvalue) + "\t" + str(CNV_kstest_pvalue) + "\n")
-		
-		
-		
-	out.close()
+		out.close()
 		
 		
 		
 
 
 if __name__=='__main__':
-	main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff, window_permutations, out_file)
+	main(ref_file, gapfile, CNV_file, variant_permutations, vcf_file, qualCutoff, window_permutations, sample_id, out_dir)
 
 
 
